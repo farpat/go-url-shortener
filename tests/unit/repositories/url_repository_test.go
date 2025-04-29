0296dcf8a0d32458c9626925c817322fc96fcc4b
Patrick FARRUGIA <patrick.farrugia@heyteam.com>
2025-04-29 15:53:00 +0200

    test(url repository): add some tests

--- tests/unit/repositories/url_repository_test.go
+++ tests/unit/repositories/url_repository_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/farpat/go-url-shortener/internal/config"
 	"github.com/farpat/go-url-shortener/internal/models"
-	"github.com/farpat/go-url-shortener/internal/repositories"
+	urlRepository "github.com/farpat/go-url-shortener/internal/repositories"
 	"github.com/farpat/go-url-shortener/internal/utils"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
@@ -59,13 +59,85 @@ func TestAll(t *testing.T) {
 	for _, url := range urls {
 		insertUrl(db, url)
 	}
-	allUrls, _ := repositories.All()
+	allUrls, _ := urlRepository.All()
 
 	// ASSERT
 	assert.Equal(t, 2, len(allUrls))
+	assert.Equal(t, 2, getUrlsCount(db))
 
 	expectedSlugs := map[string]bool{"abc": true, "def": true}
 	for _, u := range allUrls {
 		assert.True(t, expectedSlugs[u.Slug])
 	}
+}
+
+func TestFind(t *testing.T) {
+	// ARRANGE
+	teardown, db := setupTestDB()
+	defer teardown()
+
+	// ACT
+	url := models.UrlShowItem{Slug: "abc", Url: "https://example.com"}
+	insertUrl(db, url)
+	foundUrl, _ := urlRepository.Find(url.Slug)
+
+	// ASSERT
+	assert.Equal(t, url.Slug, foundUrl.Slug)
+	assert.Equal(t, url.Url, foundUrl.Url)
+	assert.Equal(t, 1, getUrlsCount(db))
+}
+
+func TestFindNotFound(t *testing.T) {
+	// ARRANGE
+	teardown, db := setupTestDB()
+	defer teardown()
+	var oldUrlCounts int
+	db.QueryRow("SELECT COUNT(*) FROM urls").Scan(&oldUrlCounts)
+
+	// ACT
+	_, err := urlRepository.Find("not-found")
+
+	// ASSERT
+	expectedError := urlRepository.NotFoundError{Slug: "not-found"}
+	assert.EqualError(t, err, expectedError.Error())
+	var newUrlCounts int
+	db.QueryRow("SELECT COUNT(*) FROM urls").Scan(&newUrlCounts)
+	assert.Equal(t, oldUrlCounts, newUrlCounts)
+}
+
+func TestDelete(t *testing.T) {
+	// ARRANGE
+	teardown, db := setupTestDB()
+	defer teardown()
+
+	// ACT
+	url := models.UrlShowItem{Slug: "abc", Url: "https://example.com"}
+	insertUrl(db, url)
+	err := urlRepository.Delete(url.Slug)
+
+	// ASSERT
+	assert.NoError(t, err)
+	assert.Equal(t, 0, getUrlsCount(db))
+}
+
+func TestDeleteNotFound(t *testing.T) {
+	// ARRANGE
+	teardown, db := setupTestDB()
+	defer teardown()
+
+	// ACT
+	url := models.UrlShowItem{Slug: "abc", Url: "https://example.com"}
+	insertUrl(db, url)
+	err := urlRepository.Delete("not-found")
+
+	// ASSERT
+	expectedError := urlRepository.NotFoundError{Slug: "not-found"}
+	assert.EqualError(t, err, expectedError.Error())
+	assert.Equal(t, 1, getUrlsCount(db))
+}
+
+func getUrlsCount(db *sql.DB) int {
+	var count int
+	db.QueryRow("SELECT COUNT(*) FROM urls").Scan(&count)
+	return count
 }
